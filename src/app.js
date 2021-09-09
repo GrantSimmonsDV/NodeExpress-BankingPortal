1dbb55fdc6a75b9490952057d0543c80f9cec394
GrantSimmonsDV <gsimmons.dv@gmail.com>
2021-09-09 13:45:52 -0600

    firstofDay

--- src/app.js
+++ src/app.js
@@ -7,15 +7,20 @@ const app = express();
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
+//Direct express to the public folder. Configure static directory
 app.use(express.static(path.join(__dirname, '/public')));
 
-const accountData = fs.readFileSync('/src/json/accounts.json', {encoding:'utf8'})
+
+//Read account data
+const accountData = fs.readFileSync('/src/json/accounts.json', {encoding:'utf8'});
 const accounts = JSON.parse(accountData);
 
+//Create the Index route
 app.get('/', (req, res) => {
     res.render('index', {title: 'Index'})
 })
 
+//Create server
 app.listen(3000, () => {
     console.log('PS Project Running on port 3000!')
 });
