e8924f49fa763523387cd8646b030a9cdf940179
GrantSimmonsDV <gsimmons.dv@gmail.com>
2021-09-09 15:05:31 -0600

    hopefully

--- src/app.js
+++ src/app.js
@@ -1,26 +1,53 @@
-const fs = require ('fs');
-const path = require ('path');
-const express = require ('express')
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 
 const app = express();
 
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
 
 //Direct express to the public folder. Configure static directory
-app.use(express.static(path.join(__dirname, '/public')));
-
+app.use(express.static(path.join(__dirname, "/public")));
 
 //Read account data
-const accountData = fs.readFileSync('/src/json/accounts.json', {encoding:'utf8'});
+const accountData = fs.readFileSync("/src/json/accounts.json", {
+  encoding: "utf8",
+});
 const accounts = JSON.parse(accountData);
 
+//Read User data
+const userData = fs.readFileSync("src/json/users.json", {
+  encoding: "utf8",
+});
+const users = JSON.parse(userData);
+
+//Create the Savings Account Route
+app.get("/savings", (req, res) => {
+  res.render("account", { account: accounts.savings });
+});
+
+//Create the Checking Route
+app.get("/checking", (req, res) => {
+  res.render("account", { account: accounts.checking });
+});
+
+//Create the Credit Route
+app.get("/credit", (req, res) => {
+  res.render("account", { account: accounts.credit });
+});
+
+//Create Profile Route
+app.get("/profile", (req, res) => {
+  res.render("profile", { user: users[0] });
+});
+
 //Create the Index route
-app.get('/', (req, res) => {
-    res.render('index', {title: 'Index'})
-})
+app.get("/", (req, res) => {
+  res.render("index", { title: "Account Summary", account: accounts });
+});
 
 //Create server
 app.listen(3000, () => {
-    console.log('PS Project Running on port 3000!')
+  console.log("PS Project Running on port 3000!");
 });
